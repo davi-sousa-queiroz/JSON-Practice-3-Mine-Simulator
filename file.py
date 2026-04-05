54158d573e2d40f55f4fb4de92cb30bc5914670f
Davi Queiroz <daviqueiroz26@proton.me>
2026-04-05 16:42:30 -0300

    add load method

--- file.py
+++ file.py
@@ -4,4 +4,21 @@ class File:
 
     def __innit__(self, file_name):
 
-        self.file_name = file_name
+        self.file_path = file_path
+
+    def load(self):
+        try:
+            with open(self.file_path, "r") as file:
+                return json.load(file)
+        except FileNotFoundError:
+            return {
+                "Coins": 0,
+                "Coal": 0,
+                "Copper Ore": 0,
+                "Iron Ore": 0,
+                "Gold Ore": 0,
+                "Diamonds": 0,
+                "Copper Bar": 0,
+                "Iron Bar": 0,
+                "Gold Bar": 0,
+            }
