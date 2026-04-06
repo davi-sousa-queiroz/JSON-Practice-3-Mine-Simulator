581871a1ee60411d7bc0d05cf4f941ac0d5c232b
Davi Queiroz <daviqueiroz26@proton.me>
2026-04-06 13:30:25 -0300

    add menu method to Economy class

--- economy.py
+++ economy.py
@@ -3,3 +3,12 @@ class Economy:
     def __init__(self, data):
 
         self.data = data
+
+    def menu(self):
+
+        print("\n============ SELL =============")
+        print("1. | Coal       | (1$)   | ")
+        print("2. | Copper Ore | (5$)   |")
+        print("3. | Iron Ore   | (10$)  |")
+        print("4. | Gold Ore   | (50$)  |")
+        print("5. | Diamond    | (250$) |")
